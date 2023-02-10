fadfe472aa583ab215cd9b377933470bdbaa6adc
SimoLomba53 <simolomba253@gmail.com>
2023-02-10 16:18:29 +0100

    immagini che cambiano al click

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 
-    const {createApp} = Vue
+    const {createApp} = Vue;
 
     createApp({
         data() {
       return {
+        movie:{
+              titolo: "Lorem, ipsum dolor sit amet consectetur adipisicing elit expedita voluptatum unde laboriosam molestiae.",
+            
+              testo: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Iste deserunt doloremque hic eaque culpa aspernatur",
+
+              images:[
+                "./img/01.webp",
+                "./img/02.webp",
+                "./img/03.webp",
+                "./img/04.webp",
+                "./img/05.webp"
+            ],
+         
+            activeImage:0 
+
+        }
         
       }
     },
 
     methods:{
+        prevImage(){
+           this.movie.activeImage--;
+        },
 
+
+        nextImage(){
+           this.movie.activeImage++;
+        },
     },
 
     created(){
@@ -19,5 +42,5 @@
     mounted(){
 
     }
-    
+
   }).mount('#app')
